7a4b07b30ba18d376caf675603607b4184e60f3a
Eddy Jansson <eddy@klopper.net>
2023-02-28 17:50:26 +0100

    Always allocate zt in output of SDL_iconv_string()
    Before this, the function could not be used on buffers,
    as it would not account for the zero-termination unless
    it was included in the input.
    
    (cherry picked from commit 5f5abb680523e1adedd3fca2a8e252db01fc1c52)

--- src/stdlib/SDL_iconv.c
+++ src/stdlib/SDL_iconv.c
@@ -833,7 +833,7 @@ SDL_iconv_string(const char *tocode, const char *fromcode, const char *inbuf,
     }
 
     stringsize = inbytesleft > 4 ? inbytesleft : 4;
-    string = (char *) SDL_malloc(stringsize);
+    string = (char *) SDL_malloc(stringsize + 1);
     if (!string) {
         SDL_iconv_close(cd);
         return NULL;
@@ -850,7 +850,7 @@ SDL_iconv_string(const char *tocode, const char *fromcode, const char *inbuf,
             {
                 char *oldstring = string;
                 stringsize *= 2;
-                string = (char *) SDL_realloc(string, stringsize);
+                string = (char *) SDL_realloc(string, stringsize + 1);
                 if (!string) {
                     SDL_free(oldstring);
                     SDL_iconv_close(cd);
@@ -877,6 +877,7 @@ SDL_iconv_string(const char *tocode, const char *fromcode, const char *inbuf,
             break;
         }
     }
+    *outbuf = '\0';
     SDL_iconv_close(cd);
 
     return string;
