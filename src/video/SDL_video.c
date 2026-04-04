ce3cc80aca6e2d7eab0865ba9e39d3ac5855794a
Sam Lantinga <slouken@libsdl.org>
2026-04-04 10:07:58 -0700

    Send SDL_EVENT_SCREEN_KEYBOARD_HIDDEN when the keyboard is hidden on Android
    (cherry picked from commit fc2f4fcc225d0c61eb8155245b05fe8c0a01454b)

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -5913,7 +5913,7 @@ bool SDL_ScreenKeyboardShown(SDL_Window *window)
 
 void SDL_SendScreenKeyboardShown(void)
 {
-    if (_this->screen_keyboard_shown) {
+    if (!_this || _this->screen_keyboard_shown) {
         return;
     }
 
@@ -5929,7 +5929,7 @@ void SDL_SendScreenKeyboardShown(void)
 
 void SDL_SendScreenKeyboardHidden(void)
 {
-    if (!_this->screen_keyboard_shown) {
+    if (!_this || !_this->screen_keyboard_shown) {
         return;
     }
 
